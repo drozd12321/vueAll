133168a5e38899b41b02e33cd427b075640f78e8
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-24 18:16:55 +0200

    2

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,34 +1,89 @@
 <template>
-  <div class="glavn">
+  <form class="glavn">
     <div class="inf">
-      <h2>Авторизация</h2>
-      <div>hh</div>
-      <div>fff</div>
+      <h2>Войти в систему</h2>
+      <div class="inpdiv">
+        <label class="label" for="email">Email:</label>
+        <input class="input" type="email" id="email" />
+      </div>
+
+      <div class="inpdiv">
+        <label class="label" for="email">Password:</label>
+        <input class="input" type="password" id="password" />
+      </div>
+      <button>Войти</button>
     </div>
-  </div>
+  </form>
 </template>
 <script setup lang="ts"></script>
 <style scoped>
+button {
+  padding: 7px;
+  font-size: 15px;
+  width: 100px;
+  border: 3px solid rgba(255, 255, 255, 0.911);
+  background-color: transparent;
+  color: white;
+  border-radius: 4px;
+  transition: all 0.4s ease;
+}
+button:hover {
+  background-color: rgba(85, 83, 83, 0.61);
+}
+.inpdiv {
+  display: grid;
+  grid-template-areas: "label input";
+  width: 400px;
+  font-size: 18px;
+  align-items: center;
+  margin-bottom: 2rem;
+}
+.label {
+  grid-area: label;
+  width: 20px;
+}
+.input {
+  grid-area: input;
+  width: 100%;
+}
+input {
+  padding: 6px;
+  border-radius: 6px;
+  outline: none;
+  background-color: transparent;
+  color: white;
+  border: 2px solid white;
+  font-size: 14px;
+}
 .glavn {
-  border: 3px solid rgb(255, 255, 255);
+  border: 3px solid rgba(0, 225, 255, 0.63);
   background-color: rgb(36, 38, 59);
   margin: 1rem auto;
   width: 80%;
   padding: 10px;
   border-radius: 6px;
   color: rgb(255, 252, 252);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80vh;
 }
 .inf {
   display: flex;
   flex-direction: column;
   align-items: center;
   font-weight: 600;
+  box-shadow: 0 5px 10px rgba(247, 245, 245, 0.2);
+  width: 600px;
+  height: 300px;
+  border-radius: 3px;
+  border: 3px solid rgba(0, 225, 255, 0.63);
 }
 h2 {
-  text-align: center;
   background-color: transparent;
   padding: 7px;
   border-radius: 7px;
+  margin-bottom: 3rem;
   box-shadow: 0 10px 8px rgba(0, 0, 0, 0.5);
 }
 </style>
