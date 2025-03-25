c5a9c066ee8cf9961ccebff762d488e3777b3c43
Drozdov Evgeny <drozdove912@gmail.com>
2025-03-25 18:19:33 +0200

    2

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,9 @@
 import { createStore } from "vuex/types/index.js";
 import authModule from "./modules/auth.module";
 
-interface RootSt {
-  version: string;
-}
 export const store = createStore({
   modules: {
     auth: authModule,
-  },
-  state(): RootSt {
-    return {
-      version: "222",
-    };
   },
   mutations: {},
   actions: {},
